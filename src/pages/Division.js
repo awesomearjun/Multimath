a4e59d47b829715ab908562ad17a359a5c4664bc
awesomearjun <iamawesomearjun@gmail.com>
2022-10-14 19:28:33 -0400

    Fixed bug where num1 / num2 = decimal

--- src/pages/Division.js
+++ src/pages/Division.js
@@ -3,10 +3,24 @@ import generateNumber from "../components/GenerateNumber"
 import LinkToHome from "../components/LinkToHome"
 
 function Division() {
-    let numbers = [generateNumber(13), generateNumber(13)];
+    const numbers = [generateNumber(13), generateNumber(13)];
+    const isInteger = num => {
+        return num % 1 === 0;
+    }
 
+    // If the first number is greater than the second, make the first number 12
     if (numbers[0] < numbers[1]) {
         numbers[0] = 12;
+        console.log(numbers[0]);
+    }
+
+    while (!isInteger(numbers[0] / numbers[1])) {
+        numbers[1] -= 1;
+
+        if (numbers[1] <= 0) {
+            numbers[0] = 12;
+            numbers[1] = 6;
+        }
     }
 
     return (
